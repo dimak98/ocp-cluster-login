b4e73cc5563b75ebffbbed0f9fd973f608b3e03e
dimak98 <kazindevops@gmail.com>
2023-12-23 19:09:26 +0200

    Update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ocp-cluster-login',
-    version='1.0.0',
+    version='v1.0.0',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
